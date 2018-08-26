75565268fa731b80c430f9866e8c396e4c84f5b7
Andre <andre_dean@me.com>
2018-08-26 16:29:31 -0700

    Setup firebase. Create RoomList component with static rooms created in firebase

--- chitchat/src/App.js
+++ chitchat/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBBE-QREcBXFpMbaUjxFV1IcxlG33YnvGY",
+  authDomain: "chitchat-87d11.firebaseapp.com",
+  databaseURL: "https://chitchat-87d11.firebaseio.com",
+  projectId: "chitchat-87d11",
+  storageBucket: "chitchat-87d11.appspot.com",
+  messagingSenderId: "489599194815"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <RoomList 
+          firebase={firebase} 
+        />
       </div>
     );
   }
